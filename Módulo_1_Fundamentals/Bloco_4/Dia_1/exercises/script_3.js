6a4438f4a6548a548e567e7166764d01470d2d99
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-22 17:45:03 -0300

    Conclui o Exercício 3

---
+++ Módulo_1_Fundamentals/Bloco_4/Dia_1/exercises/script_3.js
@@ -0,0 +1,47 @@
+//Exercício 3
+
+let a = 35;
+let b = 13;
+let c = 52;
+
+if (a > b && a > c) {
+  console.log ("O número " + a + " é o maior.");
+} else if (b > a && b > c) {
+  console.log ("O número " + b + " é o maior.");
+} else if (c > a && c > b) {
+  console.log ("O número " + c + " é o maior.");
+} else {
+  console.log ("Os três números são iguais.");
+}
+
+//Testando números iguais
+
+a = 20;
+b = 20;
+c = 20;
+
+if (a > b && a > c) {
+  console.log ("O número " + a + " é o maior.");
+} else if (b > a && b > c) {
+  console.log ("O número " + b + " é o maior.");
+} else if (c > a && c > b) {
+  console.log ("O número " + c + " é o maior.");
+} else {
+  console.log ("Os três números são iguais.");
+}
+
+//Testando novos números
+
+a = 45;
+b = 62;
+c = 35;
+
+if (a > b && a > c) {
+  console.log ("O número " + a + " é o maior.");
+} else if (b > a && b > c) {
+  console.log ("O número " + b + " é o maior.");
+} else if (c > a && c > b) {
+  console.log ("O número " + c + " é o maior.");
+} else {
+  console.log ("Os três números são iguais.");
+}
