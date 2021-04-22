6e750a24aecc212070387e8531aec7f532d9ee5d
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-22 17:51:18 -0300

    Conclui o Exercício 4

---
+++ Módulo_1_Fundamentals/Bloco_4/Dia_1/exercises/script_4.js
@@ -0,0 +1,37 @@
+//Exercício 4
+
+//Testando variável com valor positivo
+
+let a = 13;
+
+if (a > 0) {
+  console.log ("Positive")
+} else if (a < 0) {
+  console.log ("Negative")
+} else {
+  console.log ("Zero")
+}
+
+//Testando variável com valor negativo
+
+a = -24;
+
+if (a > 0) {
+  console.log ("Positive")
+} else if (a < 0) {
+  console.log ("Negative")
+} else {
+  console.log ("Zero")
+}
+
+//Testando variável com valor zero
+
+a = 0;
+
+if (a > 0) {
+  console.log ("Positive")
+} else if (a < 0) {
+  console.log ("Negative")
+} else {
+  console.log ("Zero")
+}
