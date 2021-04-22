8e1dd525453bf53fd12eacf8cca75b762ec7f4bc
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-22 10:52:07 -0300

    Exercício de Fixação - Parte IV do Conteúdo

---
+++ Módulo_1_Fundamentals/Bloco_4/Dia_1/content-exercises/scripts_3.js
@@ -0,0 +1,38 @@
+//Primeiro teste - Atribuindo valor inicial à variável notaCandidato
+
+let notaCandidato = 78;
+if (notaCandidato >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaCandidato >= 60 && notaCandidato < 80) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
+
+//Segundo teste - Atribuindo novo valor à variável notaCandidato
+
+notaCandidato = 95;
+if (notaCandidato >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaCandidato >= 60 && notaCandidato < 80) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
+
+//Terceiro teste - Atribuindo novo valor à variável notaCandidato
+
+notaCandidato = 53;
+if (notaCandidato >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (notaCandidato >= 60 && notaCandidato < 80) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
