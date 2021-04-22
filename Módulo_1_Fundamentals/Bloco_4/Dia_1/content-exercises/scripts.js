9512e14011db3d582e3bcd2d9bdc51d1e029aa26
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-22 10:19:17 -0300

    Exercício de Fixação - Parte II do Conteúdo

---
+++ Módulo_1_Fundamentals/Bloco_4/Dia_1/content-exercises/scripts.js
@@ -0,0 +1,8 @@
+const name = "Thalles";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1993;
+birthYear = 2030;
+birthCity = "Guarapari";
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
