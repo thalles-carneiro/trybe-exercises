a487f3e6bd8afda23cbc85cc1d7ba04091f045ae
Thalles Carneiro <thallescarneiro@gmail.com>
2021-04-22 20:28:45 -0300

    Conclui o Exercício 7

---
+++ Módulo_1_Fundamentals/Bloco_4/Dia_1/exercises/script_7.js
@@ -0,0 +1,101 @@
+//Exercício 7
+
+//Testando uma nota de 85% => Nota B
+
+let notaPorcentagem = 85;
+
+if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+  console.log ("Nota A");
+} else if (notaPorcentagem >= 80) {
+  console.log ("Nota B");
+} else if (notaPorcentagem >= 70) {
+  console.log ("Nota C");
+} else if (notaPorcentagem >= 60) {
+  console.log ("Nota D");
+} else if (notaPorcentagem >= 50) {
+  console.log ("Nota E");
+} else if (notaPorcentagem < 50 && notaPorcentagem >= 0) {
+  console.log ("Nota F");
+} else {
+  console.log ("ERRO!!! A nota não é válida. Insira uma nota válida.");
+}
+
+//Testando uma nota de 62% => Nota D
+
+notaPorcentagem = 62;
+
+if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+  console.log ("Nota A");
+} else if (notaPorcentagem >= 80) {
+  console.log ("Nota B");
+} else if (notaPorcentagem >= 70) {
+  console.log ("Nota C");
+} else if (notaPorcentagem >= 60) {
+  console.log ("Nota D");
+} else if (notaPorcentagem >= 50) {
+  console.log ("Nota E");
+} else if (notaPorcentagem < 50 && notaPorcentagem >= 0) {
+  console.log ("Nota F");
+} else {
+  console.log ("ERRO!!! A nota não é válida. Insira uma nota válida.");
+}
+
+//Testando uma nota de 47% => Nota F
+
+notaPorcentagem = 47;
+
+if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+  console.log ("Nota A");
+} else if (notaPorcentagem >= 80) {
+  console.log ("Nota B");
+} else if (notaPorcentagem >= 70) {
+  console.log ("Nota C");
+} else if (notaPorcentagem >= 60) {
+  console.log ("Nota D");
+} else if (notaPorcentagem >= 50) {
+  console.log ("Nota E");
+} else if (notaPorcentagem < 50 && notaPorcentagem >= 0) {
+  console.log ("Nota F");
+} else {
+  console.log ("ERRO!!! A nota não é válida. Insira uma nota válida.");
+}
+
+//Testando uma nota de 143% => Nota Inválida
+
+notaPorcentagem = 143;
+
+if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+  console.log ("Nota A");
+} else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
+  console.log ("Nota B");
+} else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
+  console.log ("Nota C");
+} else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
+  console.log ("Nota D");
+} else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
+  console.log ("Nota E");
+} else if (notaPorcentagem < 50 && notaPorcentagem >= 0) {
+  console.log ("Nota F");
+} else {
+  console.log ("ERRO!!! A nota não é válida. Insira uma nota válida.");
+}
+
+//Testando uma nota de -37% => Nota Inválida
+
+notaPorcentagem = -37;
+
+if (notaPorcentagem >= 90 && notaPorcentagem <= 100) {
+  console.log ("Nota A");
+} else if (notaPorcentagem >= 80 && notaPorcentagem < 90) {
+  console.log ("Nota B");
+} else if (notaPorcentagem >= 70 && notaPorcentagem < 80) {
+  console.log ("Nota C");
+} else if (notaPorcentagem >= 60 && notaPorcentagem < 70) {
+  console.log ("Nota D");
+} else if (notaPorcentagem >= 50 && notaPorcentagem < 60) {
+  console.log ("Nota E");
+} else if (notaPorcentagem < 50 && notaPorcentagem >= 0) {
+  console.log ("Nota F");
+} else {
+  console.log ("ERRO!!! A nota não é válida. Insira uma nota válida.");
+}
