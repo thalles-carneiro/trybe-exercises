26da4c3d97370daa9f8a55c15ef19dd42f3fda7f
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-03 23:50:39 -0300

    Exercício 2

--- Módulo_1_Fundamentals/Bloco_5/Dia_3/exercises/script.js
+++ Módulo_1_Fundamentals/Bloco_5/Dia_3/exercises/script.js
@@ -13,7 +13,6 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-
 function createzDaysOfTheMonth() {
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const getMonthDaysList = document.querySelector('#days');
@@ -34,3 +33,14 @@ function createzDaysOfTheMonth() {
 };
 
 createzDaysOfTheMonth();
+
+function createHolidayButton(string) {
+  const getButtonsContainer = document.querySelector('.buttons-container');
+  const holidayButton = document.createElement('button');
+  holidayButton.innerHTML = string;
+  holidayButton.id = 'btn-holiday';
+  getButtonsContainer.appendChild(holidayButton);
+};
+
+createHolidayButton('Feriados');
+
