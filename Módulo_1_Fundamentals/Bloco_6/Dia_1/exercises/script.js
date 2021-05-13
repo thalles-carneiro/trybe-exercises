20e437d76f4177c5480ab08ce1da0f71d428b333
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-13 14:23:45 -0300

    Exercício - Adiciona o value de cada estado

--- Módulo_1_Fundamentals/Bloco_6/Dia_1/exercises/script.js
+++ Módulo_1_Fundamentals/Bloco_6/Dia_1/exercises/script.js
@@ -1,7 +1,10 @@
-const allStatesOfBrazil = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const allInitials = ['ac', 'al', 'ap', 'am', 'ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc', 'sp', 'se', 'to'];
+const allStatesOfBrazil = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 const selectState = document.getElementById('input-state');
-for (let state of allStatesOfBrazil) {
+for (let index = 0; index < allStatesOfBrazil.length; index += 1) {
   const newState = document.createElement('option');
-  newState.innerText = state;
+  newState.innerText = allStatesOfBrazil[index];
+  newState.value = allInitials[index];
   selectState.appendChild(newState);
 }
+
