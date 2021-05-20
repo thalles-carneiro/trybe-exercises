8b761b33270f0c0f975c520d1407e48b64b15d5c
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-20 19:03:09 -0300

    Exercícios - Parte I - Realiza o exercício 2

--- Módulo_1_Fundamentals/Bloco_7/Dia_2/exercises/script_part1_01.js
+++ Módulo_1_Fundamentals/Bloco_7/Dia_2/exercises/script_part1_01.js
@@ -42,15 +42,21 @@ const customerInfo = (order) => {
   const street = order.address.street;
   const number = order.address.number;
   const apartment = order.address.apartment;
-  const finalString = `Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, R. ${street}, N°: ${number}, AP: ${apartment}.`
+  const finalString = `Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, R. ${street}, N°: ${number}, AP: ${apartment}.`;
   return console.log(finalString);
 }
 
 customerInfo(order);
+console.log('--------------');
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
-
+  const name = order.name = 'Luiz Silva';
+  const pizzas = Object.keys(order.order.pizza);
+  const drinks = order.order.drinks.coke.type;
+  const payment = order.payment.total = 50;
+  const finalString = `Olá ${name}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${drinks} é R$ ${payment},00.`;
+  return console.log(finalString);
 }
 
 orderModifier(order);
