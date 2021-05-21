9f8e1a5adb498deb475b40caa96d64e95aefc9d9
Thalles Carneiro <thallescarneiro@gmail.com>
2021-05-20 23:17:24 -0300

    Exercícios - Parte II - Realiza o exercício 5

--- Módulo_1_Fundamentals/Bloco_7/Dia_2/exercises/script_part2_01.js
+++ Módulo_1_Fundamentals/Bloco_7/Dia_2/exercises/script_part2_01.js
@@ -40,3 +40,8 @@ console.log('--------------');
 const objValues = (obj) => Object.values(obj);
 console.log(objValues(lesson1));
 console.log('--------------');
+
+//5. Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
+console.log('--------------');
