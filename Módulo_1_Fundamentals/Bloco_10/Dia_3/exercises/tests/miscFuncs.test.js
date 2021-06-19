77d17c547fe61d2babf305273d6447e08e5fe3fe
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-18 23:10:42 -0300

    Exercícios - Realiza o exercício 2

--- Módulo_1_Fundamentals/Bloco_10/Dia_3/exercises/tests/miscFuncs.test.js
+++ Módulo_1_Fundamentals/Bloco_10/Dia_3/exercises/tests/miscFuncs.test.js
@@ -3,9 +3,19 @@ const miscFuncs = require('../src/miscFuncs');
 describe('Função que gera número aleatório entre 0 e 100', () => {
   miscFuncs.randomNumber = jest.fn().mockReturnValue(10);
 
-  it('Testa sua chamada, retorno e quantas vezes foi chamada', () => {
+  it('Testa a chamada da função, seu retorno e quantas vezes foi chamada', () => {
     expect(miscFuncs.randomNumber()).toBe(10);
     expect(miscFuncs.randomNumber).toHaveBeenCalled();
     expect(miscFuncs.randomNumber).toHaveBeenCalledTimes(1);
+  });
+
+  it('Testa a chamada da função, seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
+    miscFuncs.randomNumber.mockReset();
+    miscFuncs.randomNumber.mockImplementation((a, b) => a / b);
+
+    expect(miscFuncs.randomNumber(10, 2)).toBe(5);
+    expect(miscFuncs.randomNumber).toHaveBeenCalled();
+    expect(miscFuncs.randomNumber).toHaveBeenCalledTimes(1);
+    expect(miscFuncs.randomNumber).toHaveBeenCalledWith(10, 2);
   });
 });
