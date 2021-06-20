b160ef91fcc5c91f17527bd49e3991d101c70e59
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-19 23:47:12 -0300

    Exercícios - Realiza o exercício 5

--- Módulo_1_Fundamentals/Bloco_10/Dia_3/exercises/tests/stringFuncs.test.js
+++ Módulo_1_Fundamentals/Bloco_10/Dia_3/exercises/tests/stringFuncs.test.js
@@ -1,19 +1,22 @@
 const strFuncs = require('../src/stringFuncs');
 
-jest.mock('../src/stringFuncs');
-
 describe('Testando novas implementações', () => {
   it('Mocka função para receber um parâmetro e retornar em caixa baixa', () => {
-    strFuncs.upperCase.mockImplementation((string) => string.toLowerCase());
+    const upperCase = jest.spyOn(strFuncs, 'upperCase')
+    upperCase.mockImplementation((string) => string.toLowerCase());
 
-    expect(strFuncs.upperCase('XABLAU')).toBe('xablau');
-    expect(strFuncs.upperCase).toHaveBeenCalled();
-    expect(strFuncs.upperCase).toHaveBeenCalledTimes(1);
-    expect(strFuncs.upperCase).toHaveBeenCalledWith('XABLAU');
+    expect(upperCase('XABLAU')).toBe('xablau');
+    expect(upperCase).toHaveBeenCalled();
+    expect(upperCase).toHaveBeenCalledTimes(1);
+    expect(upperCase).toHaveBeenCalledWith('XABLAU');
+
+    strFuncs.upperCase.mockRestore();
+
+    expect(strFuncs.upperCase('xablau')).toBe('XABLAU');
   });
 
   it('Mocka função para receber um parâmetro e retornar a última letra', () => {
-    strFuncs.firstCharacter.mockImplementation((string) => string.split('').reverse()[0]);
+    strFuncs.firstCharacter = jest.fn().mockImplementation((string) => string.split('').reverse()[0]);
     // service.secondFunction.mockImplementation(a => a.charAt(a.length - 1)); (GABARITO)
 
     expect(strFuncs.firstCharacter('paturso')).toBe('o');
@@ -23,7 +26,7 @@ describe('Testando novas implementações', () => {
   });
 
   it('Mocka função para receber três parâmetros e os concatena', () => {
-    strFuncs.stringConcat.mockImplementation((str1, str2, str3) => str1 + str2 + str3);
+    strFuncs.stringConcat = jest.fn().mockImplementation((str1, str2, str3) => str1 + str2 + str3);
     // service.thirdFunction.mockImplementation((a, b, c) => a.concat(b, c)); (GABARITO)
 
     expect(strFuncs.stringConcat('pato', 'urso', 'xablau')).toBe('patoursoxablau');
