8b040cd880810ebd2596cb9fc501360f1ee19594
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-23 12:18:57 -0300

    Exercício de Fixação - Composição de componentes

--- Módulo_2/Bloco_11/Dia_2/content-exercises/fixation-exercises-11-2/src/App.js
+++ Módulo_2/Bloco_11/Dia_2/content-exercises/fixation-exercises-11-2/src/App.js
@@ -1,16 +1,37 @@
-import Image from './Image';
+// arquivo App.js, criado pelo create-react-app, modificado
+import React from 'react';
+import Order from './components/Order';
 
-const imgInfos = {
-  source: 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
-  alternativeText: 'Cute cat staring',
-}
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
 
-function App() {
-  return (
-    <main>
-      <Image imgInfos={imgInfos}/>
-    </main>
-  );
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={headphone} />
+        <Order order={energyDrink} />
+      </div>
+    );
+  }
 }
 
 export default App;
