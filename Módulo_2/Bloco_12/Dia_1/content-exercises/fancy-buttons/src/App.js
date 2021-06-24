37cbf08d9eae6ba12ea02460010a4fe1c08edbb2
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-24 00:04:44 -0300

    Fixação - Componentes de Estado e eventos - Ex. 1

---
+++ Módulo_2/Bloco_12/Dia_1/content-exercises/fancy-buttons/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function handleClick() {
+  console.log('Clicou no botão!');
+}
+
+class App extends React.Component {
+  render() {
+    return (<button onClick={ handleClick }>Botão</button>);
+  }
+}
+
+export default App;
