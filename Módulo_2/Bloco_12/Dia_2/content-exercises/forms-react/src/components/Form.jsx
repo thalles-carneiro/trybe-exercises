43d91dab9159a851b4cde19612a1c93784c6967e
Thalles Carneiro <thallescarneiro@gmail.com>
2021-06-25 11:20:59 -0300

    Formulários no React - Inicia aplicação em React

---
+++ Módulo_2/Bloco_12/Dia_2/content-exercises/forms-react/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Form extends React.Component {
+  render() {
+    return (
+      <div>Form</div>
+    );
+  }
+}
+
+export default Form;
