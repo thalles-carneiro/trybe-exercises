925c5c79876e3aee0d990ba6ae43844aafc0d347
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-29 22:25:39 -0300

    Fixaçao - Testando Redux - Exemplo, refatorando

--- Módulo_2/Bloco_16/Dia_4/content-exercises/doguinhos-app/src/__tests__/App.test.js
+++ Módulo_2/Bloco_16/Dia_4/content-exercises/doguinhos-app/src/__tests__/App.test.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import event from '@testing-library/user-event';
-import '@testing-library/jest-dom/extend-expect';
 import { screen } from '@testing-library/dom';
-import fetchMock from 'fetch-mock-jest';
 import App from '../App';
 import renderWithRedux from '../helpers';
+import { END_POINT } from '../redux/actions';
 
 describe('Página inicial', () => {
-  it('Testa que o botão de adicionar cachorro está presente', () => {
+  it('Testa que o botão de adicionar cachorro está presente', async () => {
     renderWithRedux(<App />);
     const newDogBtn = screen.getByRole('button', /new dog/i);
 
     expect(newDogBtn).toBeInTheDocument();
 
-    fetchMock.getOnce('https://dog.ceo/api/breeds/image/random', {
-      body: { message: 'myDogUrl' },
-    });
+    fetch.mockResolvedValue(JSON.stringify({ message: 'myDogUrl' }));
 
     event.click(newDogBtn);
-    expect(fetchMock.called()).toBeTruthy();
+    await expect(fetch).toHaveBeenCalledTimes(1);
+    await expect(fetch).toHaveBeenCalledWith(END_POINT);
   });
 });
--- Módulo_2/Bloco_16/Dia_4/content-exercises/doguinhos-app/src/redux/actions/index.js
+++ Módulo_2/Bloco_16/Dia_4/content-exercises/doguinhos-app/src/redux/actions/index.js
@@ -1,3 +1,4 @@
+export const END_POINT = 'https://dog.ceo/api/breeds/image/random';
 export const REQUEST_IMAGE = 'REQUEST_IMAGE';
 export const GET_IMAGE = 'GET_IMAGE';
 export const FAILED_REQUEST = 'FAILED_REQUEST';
@@ -9,7 +10,6 @@ export const requestDog = () => ({ type: REQUEST_IMAGE });
 export const failedRequest = (error) => ({ type: FAILED_REQUEST, payload: error });
 
 export const fetchDogs = () => (dispatch) => {
-  const END_POINT = 'https://dog.ceo/api/breeds/image/random';
   dispatch(requestDog());
   return fetch(END_POINT)
     .then((r) => r.json())
