3e768128e1a3fcf81c1995e8883e64cf4d734f11
Thalles Carneiro <thallescarneiro@gmail.com>
2021-10-13 17:21:46 -0300

    Bônus - Node.js | Fluxo Assíncrono - Ex. [1-2]

---
+++ Back-End/Bloco_25-2/Dia_2/exercises/bonus_2.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const readline = require('readline');
+
+function question(message) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(message, (answer) => {
+      rl.close();
+  
+      resolve(answer);
+    });
+  });
+}
+
+async function start() {
+  const fileName = await question('Digite o caminho do arquivo que deseja ler: ');
+  
+  try {
+    const originalContent = await fs.readFile(fileName, 'utf-8');
+
+    const oldWord = await question('Qual palavra deseja substituir? ');
+    const newWord = await question('E qual palavra deve ficar em seu lugar? ');
+
+    const newContent = originalContent.replace(new RegExp(oldWord, 'g'), newWord);
+    console.log('Resultado da substituição: ');
+    console.log(newContent);
+
+    const destinationPath = await question('Onde deseja salvar o resultado? ');
+    await fs.writeFile(destinationPath, newContent);
+
+  } catch(err) {
+    console.error(`Erro ao ler o arquivo: ${err.path}`);
+  }
+}
+
+start();
