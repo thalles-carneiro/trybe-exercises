e751a0162783c3e78fa77a27865750d1e5d53ec4
Thalles Carneiro <thallescarneiro@gmail.com>
2022-02-09 22:00:36 -0300

    Fixação - Entrada e Saída de Dados
    Implementa arquivo content_ex_01.py que imprime um determinado nome na vertical
    Implementa arquivo content_ex_02.py que soma números inseridos por input e separados por espaço
    Implementa arquivo content_ex_03.py que imprime apenas o nome de estudantes em recuperação

---
+++ Ciencia-da-Computacao/Bloco_32/Dia_2/content_exercises/content_ex_02.py
@@ -0,0 +1,29 @@
+"""
+    Exercício 2: Escreva um programa que receba
+        vários números naturais e calcule a soma
+        desses valores. Caso algum valor da entrada
+        seja inválido, por exemplo uma letra, uma
+        mensagem deve ser exibida, na saída de erros,
+        no seguinte formato: "Erro ao somar valores,
+        {valor} é um valor inválido". Ao final, você
+        deve imprimir a soma dos valores válidos.
+
+    Dica:
+        Receba os valores em um mesmo input , solicitando
+        à pessoa usuária que separe-os com um espaço.
+        Receba os valores no formato str e utilize o método
+        split para pegar cada valor separado. O método isdigit,
+        embutido no tipo str, pode ser utilizado para verificar
+        se a string corresponde a um número natural.
+"""
+numbersStr = input("Insira valores separados por espaço: ")
+
+numbers = numbersStr.split(" ")
+sum = 0
+
+for number in numbers:
+    if number.isdigit():
+        sum += int(number)
+    else:
+        print(f"Erro ao somar valores, {number} é um valor inválido")
+print(f"A soma dos valores válidos é igual a: {sum}")
